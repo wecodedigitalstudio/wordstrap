90fc14ee64f15aefd4ba8b002584aaa5100f215f
Giacomo Corallini <giacomocorallini@Giacomos-MacBook-Pro.local>
2019-06-20 09:23:26 +0200

    aggiunto template about

---
+++ page-templates/about.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: About */ ?>
+
+<?php get_header(); ?>
+
+<section class="not-found d-flex align-items-center">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="text-monospace mt-3">About</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php  get_footer(); ?>
